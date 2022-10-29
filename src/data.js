17872a9d9994fb8670a7ffbca9f62c40a55c6376
nzoump <nzoump@gmail.com>
2022-10-29 22:10:23 +0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,83 @@
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/1.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 2,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/2.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 3,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/3.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 4,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/4.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 5,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/5.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 6,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/6.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 7,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/7.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 8,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/8.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 9,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/9.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+  {
+    id: 10,
+    name: "Lorem ipsum dolor sit amet",
+    price: "14$",
+    image: require("./images/10.jpg"),
+    details:
+      "Nullam non risus eu nisl semper congue. Nam at metus ac nulla scelerisque porttitor vel eu urna. Donec at urna nec quam commodo bibendum. Sed pulvinar tellus sapien.",
+  },
+];
+export default PRODUCTS;
